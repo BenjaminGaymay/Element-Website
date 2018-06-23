b36dff1cdb08562ed7e97dd6eee55bf5afa81501
Benjamin GAYMAY <benjamin.gaymay@epitech.eu>
2018-06-23 02:11:55 +0200

    add features

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,8 +1,9 @@
 $('a[href^="#"]').click(function(){
 	var the_id = $(this).attr("href");
+	var navbarHeight = $(".navbar").outerHeight();
 
 	$('html, body').animate({
-		scrollTop:$(the_id).offset().top
+		scrollTop:$(the_id).offset().top - navbarHeight
 	}, 'slow');
 	return false;
 });
